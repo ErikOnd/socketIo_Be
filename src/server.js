aa156ba26eb2463fe0099905cf2f5e1f2eabb06f
Erik <erik.ondra@yahoo.com>
2023-04-13 17:08:16 +0200

    first commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import mongoose from "mongoose";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { newConnectionHandler } from "./socket/index.js";
+
+const expressServer = express();
+const port = process.env.PORT || 3001;
+
+const httpServer = createServer(expressServer);
+const socketioServer = new Server(httpServer);
+
+socketioServer.on("connection", newConnectionHandler);
+
+mongoose.connect(process.env.MONGO_URL);
+
+mongoose.connection.on("connected", () => {
+  httpServer.listen(port, () => {
+    console.table(listEndpoints(expressServer));
+    console.log(`Server listening on port ${port}`);
+  });
+});
